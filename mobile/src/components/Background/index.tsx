9a874ce1f715a56066541cd24a84903d23635a30
Vinicius Perrone <perronevinicius2018@gmail.com>
2021-10-13 15:26:37 -0400

    adicionando background padrao

---
+++ mobile/src/components/Background/index.tsx
@@ -0,0 +1,21 @@
+import React, { ReactNode } from 'react';
+import { LinearGradient } from 'expo-linear-gradient';
+
+import { style } from './style';
+
+type Props = {
+  children: ReactNode;
+}
+
+const Background = ({ children }: Props) => {
+  return(
+    <LinearGradient
+      style={style.container}
+      colors={['#667BE9', '#680279', '#53197A', '#38197A', '#082F7B', '#0400D7']}
+    >
+      {children}
+    </LinearGradient>
+  )
+}
+
+export default Background;
