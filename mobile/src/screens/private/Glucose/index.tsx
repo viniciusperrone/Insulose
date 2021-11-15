b8c7fc3a4e18fda2a96bf8be58339b386f563f93
Lydia Andrade <perronevinicius2018@gmail.com>
2021-11-15 18:23:37 -0300

    Tela estática construída de glicose parte dois

--- mobile/src/screens/private/Glucose/index.tsx
+++ mobile/src/screens/private/Glucose/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity, TextInput, Touchable } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 
 import Background from '../../../components/Background';
@@ -12,21 +12,84 @@ const Glucose: React.FC = () => {
 
   const [add, setAdd] = useState(false);
 
+  function getDate(date: String) {
+    const day = date.slice(8, 10);
+    const moth = date.slice(4, 7).toLowerCase();
+    const year = date.slice(11, 15);
+    const hours = date.slice(18, 21);
+
+    let time_course = date.slice(16, 18);
+    let handleHours = '';
+    let handleTime = '';
+
+    if (Number(time_course) >= 0 && Number(time_course) <= 12) {
+      handleTime = 'AM';
+      time_course = String(Number(Number(time_course) - 12));
+      if (Number(time_course) < 10) {
+        time_course = '0' + time_course;
+      }
+    }
+    if (Number(time_course) >= 13 && Number(time_course) <= 23) {
+      handleTime = 'PM';
+      time_course = time_course;
+    }
+    return `${day} ${moth}, ${year} - ${time_course}${hours} ${handleTime}`;
+  }
+
+  const date = getDate(Date());
+
   return (
     <Background>
       <Header />
-      <View style={style.container}>
-        <Text style={style.title}>Glicemia</Text>
-        <Text style={style.textRegister}>registro: 0</Text>
-        <Text style={style.text}>
-          Você naõ possui registros. Clique em (+) para{'\n'}
-          adicionar sua primeira medição{'\n'}
-        </Text>
-        <View style={style.plusButton}>
-          <Feather name="plus" size={50} color="black" />
-        </View>
+      {
+        add
+          ?
+          <View style={style.container}>
+            <Text style={style.title}>Glicemia</Text>
+            <Text style={style.titleDate}>Data</Text>
+            <Text style={style.textDate}>{date}</Text>
+            <View>
+              <Text style={style.textContent}>
+                concentração de açucar
+              </Text>
+              <Text style={style.textContent}>
+                (mg/dL)
+              </Text>
+            </View>
+            <TextInput
+              style={style.input}
+              keyboardType={'decimal-pad'}
+            />
+            <Text style={style.textFunny}>Meu humor</Text>
+            <View style={style.containerComment}>
+              <Text style={style.textComment}>Adicionar comentário:</Text>
+              <TextInput 
+                style={style.inputComment}
+              />
+            </View>
+            <View style={style.footer}>
+              <TouchableOpacity style={[style.button, { marginLeft: 25}]} onPress={() => setAdd(false)}>
+                <Text style={style.textButton}>cancelar</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={[style.button, { marginRight: 25}]}>
+                <Text style={style.textButton}>adicionar</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+          :
+          <View style={style.container}>
+            <Text style={style.title}>Glicemia</Text>
+            <Text style={style.textRegister}>registro: 0</Text>
+            <Text style={style.text}>
+              Você naõ possui registros. Clique em (+) para{'\n'}
+              adicionar sua primeira medição{'\n'}
+            </Text>
+            <TouchableOpacity style={style.plusButton} onPress={() => setAdd(true)}>
+              <Feather name="plus" size={50} color="black" />
+            </TouchableOpacity>
 
-      </View>
+          </View>
+      }
       <Footer />
     </Background>
   );
--- mobile/src/screens/private/Glucose/style.ts
+++ mobile/src/screens/private/Glucose/style.ts
@@ -39,5 +39,79 @@ export const style = StyleSheet.create({
       marginBottom: 20,
       justifyContent: 'center',
       alignItems: 'center'
+  },
+  titleDate: {
+    fontSize: 18,
+    fontFamily: fonts.poppins_700,
+    color: colors.white,
+    alignSelf: 'center',
+  },
+  textDate: {
+    fontSize: 18,
+    fontFamily: fonts.poppins_500,
+    color: colors.white,
+    alignSelf: 'center',
+  },
+  textContent: {
+    fontSize: 15,
+    fontFamily: fonts.poppins_500,
+    color: colors.white,
+    alignSelf: 'center',
+  },
+  textFunny: {
+    fontSize: 15,
+    fontFamily: fonts.poppins_700,
+    color: colors.white,
+    alignSelf: 'center',
+  },
+  textComment: {
+    fontSize: 12,
+    fontFamily: fonts.poppins_700,
+    color: colors.white,
+    marginLeft: 50
+  },
+  containerComment: {
+      width: '100%',
+      height: 70,
+  },
+  input: {
+      width: 218,
+      height: 80,
+      backgroundColor: colors.white,
+      borderRadius: 10,
+      fontSize: 30,
+      fontFamily: fonts.poppins_500,
+      color: colors.black,
+      textAlign: 'center',
+      alignSelf: 'center'
+  },
+  inputComment: {
+    width: 320,
+    height: 40,
+    backgroundColor: colors.white,
+    borderRadius: 10,
+    fontSize: 13,
+    fontFamily: fonts.roboto_400,
+    color: colors.black,
+    alignSelf: 'center'
+  },
+  footer: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center'
+  },
+  button: {
+    width: 106,
+    height: 42,
+    backgroundColor: colors.white,
+    borderRadius: 10,
+    marginBottom: 25,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  textButton: {
+    fontSize: 15,
+    fontFamily: fonts.poppins_700,
+    color: colors.black,
   }
 });
